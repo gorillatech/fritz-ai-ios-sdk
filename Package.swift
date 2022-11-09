bb9926559796925c85b3f6d97154fe5dd90e0379
Guglielmo Faglioni <guglielmo@gorillatech.io>
2022-11-09 15:27:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FritzVisionPeopleAndPetAccurate",
-            targets: ["FritzCore", "FritzManagedModel", "FritzVisionSegmentationPredictor", "FritzVision", "FritzCoreMLHelpers", "FritzVisionPeopleAndPetAccurate"]),
+            targets: ["FritzCore", "FritzManagedModel", "FritzVisionSegmentationPredictor", "FritzVision", "FritzCoreMLHelpers", "FritzVisionPeopleAndPetSegmentationModelAccurate"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -40,7 +40,7 @@ let package = Package(
             name: "FritzCoreMLHelpers",
             path: "Source/FritzCoreMLHelpers"),
         .target(
-            name: "FritzVisionPeopleAndPetAccurate",
+            name: "FritzVisionPeopleAndPetSegmentationModelAccurate",
             dependencies: ["FritzVision"],
             path: "Source/FritzVisionPeopleAndPetSegmentationModelAccurate"
         )
